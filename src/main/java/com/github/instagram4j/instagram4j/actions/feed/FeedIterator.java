46b49414a9bc2aeb95ff144feaaed02deb9120a5
Eric Lee <c6b7yntz@gmail.com>
2024-02-14 23:33:46 +0800

    lambda

--- src/main/java/com/github/instagram4j/instagram4j/actions/feed/FeedIterator.java
+++ src/main/java/com/github/instagram4j/instagram4j/actions/feed/FeedIterator.java
@@ -10,7 +10,7 @@ public class FeedIterator<T extends IGRequest<R> & IGPaginatedRequest, R extends
         extends CursorIterator<IGRequest<R>, R> {
 
     public FeedIterator(IGClient client, T t) {
-        super(client, t, (request, id) -> t.setMax_id(id), (e) -> e.getNext_min_id(), (e) -> e.isMore_available());
+        super(client, t, (request, id) -> t.setMax_id(id), IGPaginatedResponse::getNext_min_id, IGPaginatedResponse::isMore_available);
     }
 
 }
